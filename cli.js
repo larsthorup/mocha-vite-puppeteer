94e825729a46d2be5be5af12edda7bb1022aa7ce
gabrielthomasjacobs <gjacobs@voyagersearch.com>
2021-07-12 09:22:34 -0700

    Break out options parsing to simplify cli.js

--- cli.js
+++ cli.js
@@ -2,38 +2,13 @@
 import * as fs from 'fs';
 import * as module from 'module';
 import * as path from 'path';
-import { getopt } from 'stdio';
 
 import puppeteer from 'puppeteer'
 import { createServer } from 'vite'
 
+import { options } from './optionsParser.js';
 import { MochaProtocolPlayer } from './mochaProtocolPlayer.js';
 import { MochaProtocolReporter } from './mochaProtocolReporter.js';
-
-const cliOptions = getopt({
-  port: { key: 'p', description: 'Port for the tests to run on. Default 3001', args: 1, default: undefined},
-  entry: { key: 'e', description: 'Entry html file that contains Mocha Setup. Relative to CWD, default \'test.html\'', args: 1, default: undefined},
-  reporter: { key: 'r', description: 'Reporter to use. Available: [dot, json, json-stream, list, spec, tap]. Default \'spec\'', args: 1, default: undefined},
-  reporterOptions: { key: 'o', description: 'options to pass to the reporter.', args: 1, default: undefined},
-  verbose: { key: 'v', description: 'Verbose Output'},
-  debug: { key: 'd', description: 'Enable debug mode. Note: test will run until quit via console (ctrl+c)'},
-  config: { key: 'c', description: 'Relative path to JSON config file. See project description for more details.', args: 1, default: undefined}
-});
-
-Object.keys(cliOptions).forEach(key => {
-  if(cliOptions[key] === 'undefined') { cliOptions[key] = undefined} //default values only support strings/bool
-});
-
-if(cliOptions.args) {
-  console.warn('Mocha-vite-puppeteer recieved args that don\'t match the supported syntax. Please check the github for syntax help if this was a mistake')
-};
-
-const definedProps = obj => Object.fromEntries(
-  Object.entries(obj).filter(([k, v]) => v !== undefined)
-);
-
-const jsonOptions = cliOptions.config && cliOptions.config !== 'undefined' ? JSON.parse(fs.readFileSync(cliOptions.config, 'utf-8')) : undefined;
-const options = {...jsonOptions, ...definedProps(cliOptions)};
 
 let port = Number.parseInt(options.port);
 if(!options.port || isNaN(port)){options.port = 3001; port = 3001};
