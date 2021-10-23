5ee20ec0f1b0ba16f457139fd9afac6434633c98
Lars Thorup <lars@zealake.com>
2021-10-23 20:45:38 +0200

    performance test: use typescript

---
+++ test/performance/src/generated/ComponentXXX.test.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { expect } from "chai";
+
+import ComponentXXX from "./ComponentXXX";
+
+describe("ComponentXXX", function () {
+  it("works", async function () {
+    // when: rendered
+    render(<ComponentXXX name="test-XXX" />);
+
+    // then: shows greeting without user name
+    expect(await screen.findByText("Hello from test-XXX")).to.exist;
+
+    const name = Math.random().toString();
+
+    // when: user enter name
+    userEvent.type(screen.getByRole("textbox", { name: "Enter name:" }), name);
+
+    // then: shows greeting with user name
+    expect(await screen.findByText(`Hello ${name} from test-XXX`)).to.exist;
+  });
+});
