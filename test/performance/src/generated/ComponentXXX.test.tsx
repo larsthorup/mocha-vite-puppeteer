82a4100302f27a0dc14f911c4b30ae9720f8a6c1
Lars Thorup <lars@zealake.com>
2022-02-06 15:33:10 +0100

    performance test: compare with vitest; no threads

--- test/performance/src/generated/ComponentXXX.test.tsx
+++ test/performance/src/generated/ComponentXXX.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { expect } from "chai";
 
@@ -8,17 +8,17 @@ import ComponentXXX from "./ComponentXXX";
 describe("ComponentXXX", function () {
   it("works", async function () {
     // when: rendered
-    render(<ComponentXXX name="test-XXX" />);
+    const { findByText, getByRole } = render(<ComponentXXX name="test-XXX" />);
 
     // then: shows greeting without user name
-    expect(await screen.findByText("Hello from test-XXX")).to.exist;
+    expect(await findByText("Hello from test-XXX")).to.exist;
 
     const name = Math.random().toString();
 
     // when: user enter name
-    userEvent.type(screen.getByRole("textbox", { name: "Enter name:" }), name);
+    userEvent.type(getByRole("textbox", { name: "Enter name:" }), name);
 
     // then: shows greeting with user name
-    expect(await screen.findByText(`Hello ${name} from test-XXX`)).to.exist;
+    expect(await findByText(`Hello ${name} from test-XXX`)).to.exist;
   });
 });
